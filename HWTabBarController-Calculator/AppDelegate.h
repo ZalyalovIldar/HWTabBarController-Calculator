1cf41c3d94f80bf8d9e8fe07598ba36ddb6ddd70
Ильдар Залялов <Zalyalov.Ild@gmail.com>
2016-08-16 08:48:03 +0300

    Initial Commit

---
+++ HWTabBarController-Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HWTabBarController-Calculator
+//
+//  Created by Ildar Zalyalov on 16.08.16.
+//  Copyright © 2016 com.itisiOSlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HWTabBarController-Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HWTabBarController-Calculator
+//
+//  Created by Ildar Zalyalov on 16.08.16.
+//  Copyright © 2016 com.itisiOSlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
